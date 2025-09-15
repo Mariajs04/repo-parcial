484e08e314e282b6e109f1226ec4395a7855d433
Mariajs04 <mrjsanche@gmail.com>
2025-09-15 17:02:33 -0500

    se agrego boton

--- script.js
+++ script.js
@@ -2,14 +2,7 @@ const baseURL = 'https://api.thecatapi.com/v1/'
 const endpoint = 'images/search'
 const url = `${baseURL}${endpoint}`
 
-
-/*fetch(url)
-.then(response => response.json())
-.then(datos => console.log(datos))*/
-
-
 const contenedor = document.getElementById('contenedorImagen');
-const boton = document.getElementById('nuevoGato');
 
 function cargarGato() {
   fetch(url)
@@ -20,14 +13,13 @@ function cargarGato() {
       img.src = gato.url;
       img.alt = "Un lindo gato";
       img.style.maxWidth = "300px";
-      document.body.appendChild(img);
+      
+      contenedorImagen.innerHTML = ""; 
+      contenedorImagen.appendChild(img);
   
 })}
 
-
-// Cargar un gato al iniciar la página
 cargarGato();
 
-// Cargar uno nuevo al hacer clic en el botón
-/*boton.addEventListener('click', cargarGato);*/
+
 
